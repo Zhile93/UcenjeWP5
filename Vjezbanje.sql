1de099c4c3fb84b43b835574d60e2a670fa6b975
Zhile93 <goran.zivkovic1993@gmail.com>
2024-06-15 10:48:35 +0200

    Samostalno vježbanje 15062024
    Mala vježba

---
+++ Vjezbanje.sql
@@ -0,0 +1,32 @@
+--create database ZavrsniradFondovi;
+-- use ZavrsniradFondovi;
+
+--create table Fond (
+--sifra int,
+--naziv varchar (100),
+--iznos_sredstava decimal (20,2),
+--broj_projekata int,
+--u_provedbi_do datetime
+--);
+
+
+--create table Javni_poziv (
+--sifra int, 
+--naziv varchar (100),
+--iznos_sredstava decimal (20,2),
+--stopa_sufinanciranja int,
+--traje_do datetime,
+--fond varchar (50)
+--);
+
+
+--create table Projekt (
+--sifra int,
+--javni_poziv varchar (100),
+--iznos_sredstava decimal (20,2),
+--stopa_sufinanciranja int,
+--u_provedbi bit,
+--korekcije decimal (18,2),
+--manager varchar (50),
+--provedbeno_tijelo varchar (100)
+--);
