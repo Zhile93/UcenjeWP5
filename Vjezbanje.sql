7508e0ad281177f96ec7fd520d82a1cfb4af67b3
Zhile93 <goran.zivkovic1993@gmail.com>
2024-06-15 11:33:07 +0200

    Update Vjezbanje.sql

--- Vjezbanje.sql
+++ Vjezbanje.sql
@@ -1,32 +1,32 @@
---create database ZavrsniradFondovi;
--- use ZavrsniradFondovi;
+create database ZavrsniradFondovi;
+use ZavrsniradFondovi;
 
---create table Fond (
---sifra int,
---naziv varchar (100),
---iznos_sredstava decimal (20,2),
---broj_projekata int,
---u_provedbi_do datetime
---);
-
-
---create table Javni_poziv (
---sifra int, 
---naziv varchar (100),
---iznos_sredstava decimal (20,2),
---stopa_sufinanciranja int,
---traje_do datetime,
---fond varchar (50)
---);
+create table Fond (
+sifra int,
+naziv varchar (100),
+iznos_sredstava decimal (20,2),
+broj_projekata int,
+u_provedbi_do datetime
+);
 
 
---create table Projekt (
---sifra int,
---javni_poziv varchar (100),
---iznos_sredstava decimal (20,2),
---stopa_sufinanciranja int,
---u_provedbi bit,
---korekcije decimal (18,2),
---manager varchar (50),
---provedbeno_tijelo varchar (100)
---);
+create table Javni_poziv (
+sifra int, 
+naziv varchar (100),
+iznos_sredstava decimal (20,2),
+stopa_sufinanciranja int,
+traje_do datetime,
+fond int
+);
+
+
+create table Projekt (
+sifra int,
+javni_poziv int,
+iznos_sredstava decimal (20,2),
+stopa_sufinanciranja int,
+u_provedbi bit,
+korekcije decimal (18,2),
+manager varchar (50),
+provedbeno_tijelo varchar (100)
+);
