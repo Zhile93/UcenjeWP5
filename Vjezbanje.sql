a2ea82f28d5127c98f2896737d4c44b9af24619d
Zhile93 <goran.zivkovic1993@gmail.com>
2024-07-06 11:06:43 +0200

    Update Vjezbanje.sql

--- Vjezbanje.sql
+++ Vjezbanje.sql
@@ -1,9 +1,15 @@
-create database ZavrsniradFondovi;
+use master;
+go
+drop database if exists ZavrsniradFondovi;
+go
+create database ZavrsniradFondovi collate Croatian_CI_AS;
+go
 use ZavrsniradFondovi;
 
+
 create table Fond (
-sifra int,
-naziv varchar (100),
+sifra int not null primary key identity(1,1),
+naziv varchar (100) not null,
 iznos_sredstava decimal (20,2),
 broj_projekata int,
 u_provedbi_do datetime
@@ -11,18 +17,18 @@ u_provedbi_do datetime
 
 
 create table Javni_poziv (
-sifra int, 
-naziv varchar (100),
+sifra int not null primary key identity(1,1),
+naziv varchar (100) not null,
 iznos_sredstava decimal (20,2),
 stopa_sufinanciranja int,
 traje_do datetime,
-fond int not null primary key identity
+fond int references fond(sifra)
 );
 
 
 create table Projekt (
-sifra int,
-javni_poziv int not null primary key identity,
+sifra int not null primary key identity(1,1),
+javni_poziv int references Javni_poziv(sifra),
 iznos_sredstava decimal (20,2),
 stopa_sufinanciranja int,
 u_provedbi bit,
